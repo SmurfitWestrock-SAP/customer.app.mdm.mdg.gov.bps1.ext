08a299e9258ee5052f1f17ce16acf0c1192dcfcf
MOURAD Cherifi <mourad.cherifi@smurfitwestrock.com>
2025-03-19 08:32:35 +0000

    MCH: enabled property on Create button dependent on authorization

---
+++ webapp/changes/coding/CreateButton.js
@@ -0,0 +1,20 @@
+sap.ui.define(
+    [
+        'sap/ui/core/mvc/ControllerExtension'
+    ],
+    function (
+        ControllerExtension
+    ) {
+        'use strict';
+        return ControllerExtension.extend("customer.app.mdm.mdg.gov.bps1.ext.CreateButton", {
+            override: {
+            	onBeforeRendering: function() {
+                    const oCreateButton = this.getView().byId("mdm.mdg.gov.bps1::sap.suite.ui.generic.template.ListReport.view.ListReport::BusinessPartner--Create");
+                    oCreateButton.setEnabled(false);                    
+                    oCreateButton?.bindProperty("enabled", { path: "/ZZCreationAuth(1)/Authorized" });
+                    this.getView().getModel().read("/ZZCreationAuth(1)");
+            	}
+            }
+        });
+    }
+);
